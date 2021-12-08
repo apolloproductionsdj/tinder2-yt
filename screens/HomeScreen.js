2f7163f3812e2b3bc782f1cdfbe55cabeddf1fe5
devopsdj <michael@blue360media.com>
2021-12-08 08:11:52 -0700

    created Login Screen and used ternary operator inside of StackNavigtor in order to display what I want to user if they are logged in

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>I am the Home Screen</Text>
+      <Button
+        title="Go to Chat Screen"
+        onPress={() => navigation.navigate("Chat")}
+      />
     </View>
   );
 };
