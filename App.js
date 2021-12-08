57d437a3b87d2e4e878acc0482ed93caac4bb592
devopsdj <michael@blue360media.com>
2021-12-08 07:57:11 -0700

    added in React Navigation

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import tw from "tailwind-rn";
+import StackNavigator from "./StackNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={tw("flex-1 justify-center items-center")}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
